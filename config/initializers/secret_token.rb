5f0c8f2dd69186e157bd7b6afff00d11d5adf42b
vvdpzz <vvdpzz@gmail.com>
2011-12-11 19:16:27 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trace::Application.config.secret_token = '350fd0b7c56f316cbff165091a4b3d70258141f705970a1a59d93dcdc7f0a68b8fd03c140e72ccd598a4e527b7223b704568405e53bbe4c44659e413f3067ea7'
